3d06eb248787648ab33b9f2186c363f143bb844c
Faiz Ul Hassan <faizdevhub@gmail.com>
2025-06-27 20:03:27 +0500

    main.py
    This is a main code file of my Project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # simple-basic-projects
 A collection of simple Python practice projects
+hello how are you
---
+++ main.py
@@ -0,0 +1,2 @@
+print("This is my first git hub project that i will push it today")
+print("And then i will make some changes to it")
