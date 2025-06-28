74350aaf7ce30a4d443c8b2e094d67f393416684
Faiz Ul Hassan <faizdevhub@gmail.com>
2025-06-28 21:24:57 +0500

    update hello.py with new changes

--- 01_python/hello.py
+++ 01_python/hello.py
@@ -1,2 +1,3 @@
 print("hello word!! this is my first python program line and i will be very happy...")
 print("how would like to update your first python program?")
+print("Now! i am ready to use git hub for future projects!")
